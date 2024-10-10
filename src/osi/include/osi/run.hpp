664217774210a0547bba8b0661fadcf940731f08
Martin Štourač <525032@mail.muni.cz>
2024-10-10 15:21:04 +0200

    add documentation

--- src/osi/include/osi/run.hpp
+++ src/osi/include/osi/run.hpp
@@ -4,8 +4,20 @@
 namespace osi
 {
 
+/**
+ * Initializes environment which communicates directly with the operating system.
+ */
 void start();
+
+/**
+ * Runs the application loop, in which input and output events are registered and generated respectively.
+ * @exception Throws runtime_error if glad fails to load.
+ */
 void run();
+
+/**
+ * Terminates the program and frees all the resources reserved in the start() function.
+ */
 void finish();
 
 } // namespace osi
